a3fd5fdc8ae4c178f122e8687b86641d9d11f716
gregolator <gweiser54@icloud.com>
2024-02-25 21:37:22 +0100

    Initial Commit

---
+++ website/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+from flask_session import Session
+import sqlite3
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "i am so secret"
+    app.config['SESSION_TYPE'] = "filesystem"
+    app.config['SESSION_PERMANENT'] = False
+
+    Session(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    return app
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn    
---
+++ website/views.py
@@ -0,0 +1,4 @@
+from . import get_db_connection
+from flask import Blueprint
+
+views = Blueprint('views', __name__) 
