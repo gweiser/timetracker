aa28b84ef7e3c13b3aebdd5bd2412769b50b3001
gregolator <gweiser54@icloud.com>
2024-02-27 23:01:21 +0100

    Add html for new entry, start on starting logic

--- website/init_db.py
+++ website/init_db.py
@@ -2,8 +2,30 @@ import sqlite3
 
 db = sqlite3.connect('database.db')
 
-with open("schema.sql") as f:
-    db.executescript(f.read())
+db.executescript(
+    """
+        DROP TABLE IF EXISTS entries;
+
+        CREATE TABLE entries (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            creation_date TEXT NOT NULL,
+            start_time TEXT NOT NULL,
+            end_time TEXT NOT NULL,
+            duration INTEGER NOT NULL,
+            note TEXT NOT NULL,
+            wage INTEGER NOT NULL,
+            pay INTEGER NOT NULL,
+            block_id INTEGER NOT NULL,
+            FOREIGN KEY(block_id) REFERENCES blocks(id)
+        );
+
+        DROP TABLE IF EXISTS blocks;
+
+        CREATE TABLE blocks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+        );
+    """
+)
 
 db.commit()
 db.close()
--- website/views.py
+++ website/views.py
@@ -1,7 +1,16 @@
 from . import get_db_connection
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect
+from datetime import date, datetime
 
 views = Blueprint('views', __name__) 
 
-#views.route("/", methods=["GET", "POST"])
+@views.route("/entry", methods=["GET", "POST"])
+def entry():
+    return render_template("entry.html")
 
+@views.route("/start", methods=["GET, POST"])
+def start():
+    current_date = date.today().strftime("%d/%m/%Y")
+    starttime = datetime.now().strftime("%H:%M")
+
+    return render_template("entry.html", current_date=current_date, starttime=starttime)
