2a138f0b4de3f27c9b9141485313e60f33faee21
gregolator <gweiser54@icloud.com>
2024-02-28 15:11:40 +0100

    Starttime, endtime, timeworked all work

--- website/views.py
+++ website/views.py
@@ -8,12 +8,26 @@ views = Blueprint('views', __name__)
 def entry():
     return render_template("entry.html")
 
-@views.route("/start", methods=["GET", "POST"])
-def start():
+@views.route("/startstop", methods=["GET", "POST"])
+def startstop():
     if request.method == "POST":
-        current_date = date.today().strftime("%Y-%m-%d")
-        print(current_date)
-        starttime = datetime.now().strftime("%H:%M")
-        print(starttime)
-
-        return render_template("entry.html", current_date=current_date, starttime=starttime)
+        current_date = request.form.get("date")
+        starttime = request.form.get("starttime")
+        # Start process
+        if len(starttime) == 0:
+            current_date = date.today().strftime("%Y-%m-%d")
+            starttime = datetime.now().strftime("%H:%M")
+            endtime = 0
+            worktime = 0
+        else:
+            endtime = datetime.now().strftime("%H:%M")
+            startime_split = starttime.split(":")
+            startime_format = int(startime_split[0] + startime_split[1])
+            endtime_split = endtime.split(":")
+            endtime_format = int(endtime_split[0] + endtime_split[1])
+            # Calculate difference, format it to have 4 digits
+            difference = str(endtime_format - startime_format).zfill(4)
+            # Convert to time format
+            worktime = difference[:2] + ":" + difference[2:]
+        return render_template("entry.html", current_date=current_date, starttime=starttime, endtime=endtime, worktime=worktime)
+    
