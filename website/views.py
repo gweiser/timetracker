566f4a50c7d40296213dd8d7d9922f195fbcc0c5
gregolator <gweiser54@icloud.com>
2024-03-02 15:35:23 +0100

    Progress on timedelta calculation, need to fix formatting issue

--- website/views.py
+++ website/views.py
@@ -32,11 +32,25 @@ def startstop():
                 t2 = datetime.strptime(endtime, "%H:%M")
 
                 timedelta = t2-t1
+                print(timedelta)
                 seconds = timedelta.total_seconds()
-                minutes = seconds / 60
-                hours = minutes / 60
+                minutes = int(seconds / 60)
 
-                worktime = 0
+                if minutes < 60:
+                    worktime = f"00:{(minutes)}"
+                elif minutes > 59:
+                    # Hours with comma
+                    total = str(round((minutes / 60), 2))
+                    # Just hours
+                    hours = total.split(".")[0].zfill(2)
+                    # Minutes to the hundredth
+                    minutes_hundreds = int(total.split(".")[1]) / 100 
+                    #Remaining minutes
+                    minutes_full = int(round((minutes_hundreds * 60), 0))
+                    # Minutes formatted
+                    minutes = str(minutes_full).zfill(2)
+                    worktime = f"{hours}:{minutes}"
+
 
         return render_template("entry.html", current_date=current_date, starttime=starttime, endtime=endtime, worktime=worktime)
     
